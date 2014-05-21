fd277f8b8979c65972ecd1f4afe00e4af26318d3
Aaron None <masteraero@gmail.com>
2014-05-21 19:21:25 -0400

    vanilla rails with specs for site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FancyApartments::Application.config.secret_key_base = '7119c543ca005fc30d8f73e2672306db821507b35080c87997c3124b399c58f9e35ca4f14cc66c814c14b53573a17e0e642b007eb77f9637b3c1fadb70933a55'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FancyApartments::Application.config.session_store :cookie_store, key: '_fancy_apartments_session'
